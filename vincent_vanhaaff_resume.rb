09c0ac3a471809658aabf9e645af8eb8fbc52995
Vincent van Haaff <vincent@vanhaaff.com>
2015-01-02 12:56:45 -0800

    activerecord fix and 2015 work updates

--- vincent_vanhaaff_resume.rb
+++ vincent_vanhaaff_resume.rb
@@ -10,7 +10,7 @@ module Skills
       :direction              => %w(User_Experience_Design Sound_Design Product_Design 
                                       Behaviour_Design Systems_Engineering Startup_Consulting
                                     ),
-      :programming_languages  => %w(Ruby Python Node PHP Scala Cpp_openFrameworks Java_Processing Actionscript3),
+      :programming_languages  => %w(Ruby objc Python Node PHP Scala Cpp_openFrameworks Java_Processing Actionscript3),
       :development_frameworks => %w(Rails Django Zend),
       :database               => %w(MySQL Postres SQLite MongoDB Redis),
       :testing                => %w(Test::Unit MiniTest RSpec Shoulda 
@@ -42,32 +42,37 @@ module Experience
                   :industry => "Event Management Company, Installation and Sound Art, Interactive Visuals",
                   :roles    => ["Creative Director"],
                   :when     => (5.year.ago...Time.now.to_s) }
-        
+
+        jobs << { :company  => "Rouxbe Cooking School",
+                  :industry => "Remote Education, Culinary Health and Wellness, Learning and Content Management System Development",
+                  :roles    => ["Chief Technical Officer"],
+                  :when     => (1.year.ago...Time.now.to_s) }
+
         jobs << { :company  => "Hybridity",
                   :industry => "Record Label, Installation and Sound Art, Interactive Visuals & Events",
                   :roles    => ["Technical Director", "Consultant"],
-                  :when     => (1.year.ago...Time.now.to_s) }
-                  
+                  :when     => (2.year.ago...1.year.ago) }
+
         jobs << { :company  => "Coverall Crew",
                   :industry => "Software as a Service, RoR Dojo", 
                   :roles    => ["Technical Design", "Technical Lead"],
-                  :when     => (3.year.ago...2.months.ago) }
-                  
+                  :when     => (3.year.ago...1.year.ago) }
+
         jobs << { :company  => "Blitzoo Games Inc.",
                   :industry => "Online Social Gaming",  
                   :roles    => ["Software Lead", "Platform Specialist"],
-                  :when     => (3.years.ago...2.year.ago) }
+                  :when     => (4.years.ago...3.year.ago) }
 
         jobs << { :company  => "Microsoft",
                   :industry => "Online Social Gaming, Platforms Expert, Computer Vision Expert",
                   :roles    => ["Software Engineer 2", "Platform Specialist", "User Experience Designer"],
-                  :when     => (4.5.years.ago..3.years.ago) }
+                  :when     => (5.5.years.ago..4.years.ago) }
 
 
         jobs << { :company  => "BigPark",
                   :industry => "Video Gaming",
                   :roles    => ["User Experience Designer", "Software Developer", "Sound Designer"],
-                  :when     => (6.years.ago..4.years.ago) }
+                  :when     => (7.years.ago..4.years.ago) }
 
         # TODO: add previous positions on request
 
@@ -85,7 +90,7 @@ class VincentVanHaaff < ActiveRecord::Base
   extend Skills
  
   has_one :passion, :through => "computing"
-  has_many :skills, :through => "experience", :include => "practice"
+  has_many :skills, :through => "experience", :source => "practice"
   has_many :projects, :through => "github", 
                           :foreign_key => "github.com/flyingoctopus"
  
@@ -112,11 +117,11 @@ class VincentVanHaaff < ActiveRecord::Base
  
   def self.inspect
     who << "
-    I am a passionate designer/developer with nearly ten years of commercial experience
+    I am a passionate technical creative and engineer with nearly ten years of commercial experience
     and full-stack skills in the design, development and maintenance of modern,
-    user centered design. I also create engaging experiences for installation and 
-    video games on both the web and console, with a strong interest in performance,
-    engagement, clean code, and a pension for enjoying one's day.
+    user centered tech driven products and services. I also create engaging experiences for installation and 
+    video games on both the web and console, with a strong interest in  performance,
+    engagement, clean efficient code, and accomplishing it with a strong team.
 
     Besides work, I am a co-founder of Vancouver Maker Faire as well as Vancouver Hackspace, 
     educator, and artist while living in Vancouver with my cat, Lord Nibbler. 
