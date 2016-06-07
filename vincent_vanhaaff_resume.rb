2eb626b84caa0ccb7db841233489a187b576614a
Vincent van Haaff <vincent@vanhaaff.com>
2016-06-07 13:46:17 -0700

    formatting

--- vincent_vanhaaff_resume.rb
+++ vincent_vanhaaff_resume.rb
@@ -7,21 +7,21 @@ require "active_record"
 module Skills
   def skills
     {
-      :direction              => %w(User_Experience_Design Product_Design Behaviour_Design 
+      :direction              => %w(User_Experience_Design Product_Design Behaviour_Design
                                       Sound_Design Systems_Engineering Data_Visualization
                                       Startup_Consulting Data_Driven_Intelligence
                                     ),
-      :programming_languages  => %w(Ruby Objc Python JS Node PHP Scala Cpp openFrameworks 
+      :programming_languages  => %w(Ruby Objc Python JS Node PHP Scala Cpp openFrameworks
                                       Java Processing C# dotNet Actionscript3 Apex
                                     ),
       :development_frameworks => %w(Rails Django Zend Drupal),
       :database               => %w(MySQL Postres SQLite MongoDB Redis Salesforce),
-      :testing                => %w(Test::Unit MiniTest RSpec Shoulda 
+      :testing                => %w(Test::Unit MiniTest RSpec Shoulda
                                       Cucumber Selenium
                                     ),
       :orms                   => %w(ActiveRecord Sequel Mongoid MongoMapper),
       :front_end              => %w(Angular React_js React Javascript CSS HTML
-                                      OpenFrameworks Processing WebGL 
+                                      OpenFrameworks Processing WebGL
                                       Backbone Angular CoffeeScript Actionscript
                                     ),
       :scm                    => %w(Git Perforce Subversion),
@@ -36,7 +36,7 @@ end
 module Experience
   def self.included(base)
     base.extend Skills
-   
+
     class << base
       def experience
         jobs = []
@@ -55,7 +55,7 @@ module Experience
                   :industry => "Non profit",
                   :roles    => ["Data Evangelist", "Salesforce Consultant", "Database Management", "Analytics and Business Intelligence"],
                   :when     => (10.months.ago...3.months.ago) }
-                  
+
         jobs << { :company  => "Newland Systems/Accent Steel Manufacturing",
                   :industry => "Brewing Equipment Manufacturer",
                   :roles    => ["Salesforce Consultant", "Database Management", "Front-End Developer"],
@@ -77,12 +77,12 @@ module Experience
                   :when     => (3.year.ago...2.year.ago) }
 
         jobs << { :company  => "Coverall Crew",
-                  :industry => "Software as a Service, RoR Dojo", 
+                  :industry => "Software as a Service, RoR Dojo",
                   :roles    => ["Technical Design", "Technical Lead"],
                   :when     => (4.year.ago...2.year.ago) }
 
         jobs << { :company  => "Blitzoo Games Inc.",
-                  :industry => "Online Social Gaming",  
+                  :industry => "Online Social Gaming",
                   :roles    => ["Software Lead", "Platform Specialist"],
                   :when     => (5.years.ago...4.year.ago) }
 
@@ -108,20 +108,20 @@ end
 
 
 class VincentVanHaaff < ActiveRecord::Base
- 
+
   include Experience
   extend Skills
- 
+
   has_one :passion, :through => "computing"
   has_many :skills, :through => "experience", :source => "practice"
-  has_many :projects, :through => "github", 
+  has_many :projects, :through => "github",
                           :foreign_key => "github.com/flyingoctopus"
- 
+
   validates_presence_of :innovation
   validates_presence_of :learning_opportunities
   validates_presence_of :friendly_team
   validates_presence_of :pair_programming
- 
+
   def self.objectives
     begin
       returning Array.new do |objs|
@@ -132,25 +132,25 @@ class VincentVanHaaff < ActiveRecord::Base
       puts e
       # TODO: retry harder
     end
-  end  
- 
+  end
+
   def self.contact
     `open mailto:##{first_name.downcase}@##{last_name.downcase}.com`
   end
- 
+
   def self.inspect
     who << "
     I am a passionate technical creative and engineer with nearly ten years of commercial experience
     and full-stack skills in the design, development and maintenance of modern,
-    user centered tech driven products and services. I also create engaging experiences for installation and 
+    user centered tech driven products and services. I also create engaging experiences for installation and
     video games on both the web and console, with a strong interest in  performance,
     engagement, clean efficient code, and accomplishing it with a strong team.
 
-    Besides work, I am a co-founder of Vancouver Maker Faire as well as Vancouver Hackspace, 
-    educator, and artist while living in Vancouver with my cat, Lord Nibbler. 
+    Besides work, I am a co-founder of Vancouver Maker Faire as well as Vancouver Hackspace,
+    educator, and artist while living in Vancouver with my cat, Lord Nibbler.
     I love climbing, cycling, good eats, and good times!"
   end
- 
+
   def self.method_missing(method, *args, &block)
     if method =~ /^reference$/
       raise StandardError, "Talk to me first!"
@@ -160,7 +160,7 @@ class VincentVanHaaff < ActiveRecord::Base
       contact
     end
   end
- 
+
   private
 
   def self.returning(value)
